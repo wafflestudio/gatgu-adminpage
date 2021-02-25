bffffbe6086d14b7b8c1f19e335f9e933e74a2c1
woohm402 <woohm404@gmail.com>
2021-02-25 22:08:00 +0900

    Pages folder added

--- src/Store/userSlice/index.js
+++ src/Store/userSlice/index.js
@@ -1,25 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  info: {
-    id: 0,
-    username: '',
-    password: '',
-    email: '',
-    first_name: '',
-    last_name: '',
-    userprofile: {
-      profile_id: 0,
-      picture: '',
-      nickname: '',
-      address: '',
-      phonenumber: '',
-    },
-    created_at: null,
-    updated_at: null,
-    is_active: false,
-  },
-  logged: false,
+  
 };
 
 const userSlice = createSlice({
