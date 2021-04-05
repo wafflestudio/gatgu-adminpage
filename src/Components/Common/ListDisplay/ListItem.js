9fef08f4ae533b004f1d69e8013fa9cee8e57b31
woohm402 <woohm404@gmail.com>
2021-04-05 21:02:53 +0900

    img alt add

--- src/Components/Common/ListDisplay/ListItem.js
+++ src/Components/Common/ListDisplay/ListItem.js
@@ -3,7 +3,7 @@ import './ListItem.scss';
 export const UserItem = ({ item }) => {
   return (
     <div className="useritem-wrapper">
-      <img src={item.picture} />
+      <img src={item.picture} alt="" />
       <p>{item.username}</p>
       <div>
         <p>모집내역</p>
